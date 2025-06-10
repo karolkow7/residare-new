b17308a87d5025ab10b6f209b8c5d7eca395be6a
karolkow7 <karol_kow@yahoo.de>
2025-06-10 20:57:50 +0200

    Delete translate.js

--- translate.js
+++
@@ -1,9 +0,0 @@
-translations.de["impressum.owner"] = "Angaben gemäß §5 TMG:\nResidare GmbH\nBergerhof 40\n44536 Lünen";
-translations.de["impressum.represented"] = "Vertreten durch:\nKarol Kowalczyk";
-translations.de["impressum.contact"] = "Kontakt:\nE-Mail: kontakt@residare.de";
-translations.de["impressum.legal"] = "Verantwortlich für den Inhalt nach §55 Abs. 2 RStV:\nKarol Kowalczyk, Anschrift wie oben";
-
-translations.pl["impressum.owner"] = "Informacje zgodnie z §5 TMG:\nResidare GmbH\nBergerhof 40\n44536 Lünen";
-translations.pl["impressum.represented"] = "Reprezentowany przez:\nKarol Kowalczyk";
-translations.pl["impressum.contact"] = "Kontakt:\nE-Mail: kontakt@residare.de";
-translations.pl["impressum.legal"] = "Odpowiedzialny za treść zgodnie z §55 ust. 2 RStV:\nKarol Kowalczyk, adres jak wyżej";
